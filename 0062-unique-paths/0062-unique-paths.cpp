30680ec596cff0ab37298eb7cadce84c3685a0c5
Divyarth Singh <div8791@gmail.com>
2023-07-21 22:58:04 +0530

    Time: 2 ms (43.70%), Space: 6.4 MB (48.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int helper(int i, int j, vector<vector<int>> &dp){
-        if(i==0 && j==0){
-            return 1;
-        }
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up = helper(i-1,j,dp);
-        int left = helper(i,j-1,dp);
-        return dp[i][j] = ( up + left);
+  int helper(int m, int n, int i, int j, vector<vector<int>> &dp){
+    if(i==0 && j==0){
+      return 1;
     }
+    if(i<0 || j<0) return 0;
+    
+    if(dp[i][j]!=-1) return dp[i][j];
+    
+    int up = helper(m,n,i-1,j,dp);
+    int down = helper(m,n,i,j-1,dp);
+    return dp[i][j] =  up + down;
+  }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        return helper(m-1,n-1,dp);
+      vector<vector<int>> dp(m,vector<int> (n,-1));
+        return helper(m,n,m-1,n-1,dp);
     }
 };
