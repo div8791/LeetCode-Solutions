21d49e7305cdfb233fe5838e3e0b52e8a614d964
Divyarth Singh <div8791@gmail.com>
2023-07-27 11:38:53 +0530

    Time: 166 ms (91.73%), Space: 58.1 MB (12.59%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long sum = accumulate(batteries.begin(),batteries.end(), 0LL);
+      priority_queue<int> pq(batteries.begin(),batteries.end());
+      while(pq.top() > sum/n){
+        sum -= pq.top();
+        pq.pop();
+        --n;
+      }
+      return sum/n;
+    }
+};
