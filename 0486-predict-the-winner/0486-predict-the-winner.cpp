addec6e7528ff20c2933af3450a7701bcc398089
Divyarth Singh <div8791@gmail.com>
2023-07-28 12:18:44 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (29.30%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-  int calculate_firstScore(vector<int>& nums, int i, int j){
+  int calculate_firstScore(vector<int>& nums, int i, int j,vector<vector<int>>& dp){
     if(i>j) return 0;
     if(i==j) return nums[i];
+    if(dp[i][j]!=-1) return dp[i][j];
+    int currScore = max(nums[i]+min(calculate_firstScore(nums,i+2,j,dp),calculate_firstScore(nums,i+1,j-1,dp)),nums[j] + min(calculate_firstScore(nums,i,j-2,dp),calculate_firstScore(nums,i+1,j-1,dp)));
     
-    int currScore = max(nums[i]+min(calculate_firstScore(nums,i+2,j),calculate_firstScore(nums,i+1,j-1)),nums[j] + min(calculate_firstScore(nums,i,j-2),calculate_firstScore(nums,i+1,j-1)));
-    
-    return currScore;
+    return dp[i][j]= currScore;
   }
   int calculate_totalScore(vector<int>& nums){
     int curr = 0;
@@ -17,7 +17,9 @@ public:
   }
     bool PredictTheWinner(vector<int>& nums) {
         int totalScore = calculate_totalScore(nums);
-      int firstScore = calculate_firstScore(nums,0,nums.size()-1);
+      int n = nums.size();
+      vector<vector<int>> dp(n,vector<int> (n,-1));
+      int firstScore = calculate_firstScore(nums,0,nums.size()-1,dp);
       return firstScore >= totalScore-firstScore;
     }
 };
